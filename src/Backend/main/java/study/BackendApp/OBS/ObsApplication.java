23038f562a67d97105cac52693620b48ababce99
Paros998 <73112078+Paros998@users.noreply.github.com>
2021-10-11 10:47:38 +0200

    reorganization v2

---
+++ src/Backend/main/java/study/BackendApp/OBS/ObsApplication.java
@@ -0,0 +1,13 @@
+package study.BackendApp.OBS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObsApplication.class, args);
+	}
+
+}
---
+++ src/Backend/test/java/study/BackendApp/OBS/ObsApplicationTests.java
@@ -0,0 +1,13 @@
+package study.BackendApp.OBS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
