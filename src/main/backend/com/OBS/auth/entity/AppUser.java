94a8d931279756908e6f3da12d34ae026d454bec
Paros998 <73112078+Paros998@users.noreply.github.com>
2021-10-31 11:19:22 +0100

    update

--- src/main/backend/com/OBS/auth/entity/AppUser.java
+++ src/main/backend/com/OBS/auth/entity/AppUser.java
@@ -10,11 +10,9 @@ import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import javax.persistence.*;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Set;
 
 @Getter
 @Setter
