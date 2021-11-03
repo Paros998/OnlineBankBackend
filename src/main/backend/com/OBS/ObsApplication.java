af89932d5567d593c295ac14b30d9373ffc94252
Paros998 <73112078+Paros998@users.noreply.github.com>
2021-11-03 13:37:58 +0100

    swagger added,cors update , endpoints redone and new antMatchers for unauthenticated users

--- src/main/backend/com/OBS/ObsApplication.java
+++ src/main/backend/com/OBS/ObsApplication.java
@@ -25,7 +25,9 @@ public class ObsApplication {
         corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5555","https://pip-frontend-server.herokuapp.com"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept",
                 "Authorization", "Origin , Accept", "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
-        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept",
+                "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS" ,"PATCH"));
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(urlBasedCorsConfigurationSource);
