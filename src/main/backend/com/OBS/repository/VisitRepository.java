458a9af97e292c2e7fe1905be1bafe2d253c8b5f
Paros998 <73112078+Paros998@users.noreply.github.com>
2021-12-02 23:17:25 +0100

    added ommited file

--- src/main/backend/com/OBS/repository/VisitRepository.java
+++ src/main/backend/com/OBS/repository/VisitRepository.java
@@ -4,7 +4,10 @@ import com.OBS.entity.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VisitRepository extends JpaRepository<Visit, Long> {
-
+    List<Visit> findAllByEmployee_EmployeeId(Long id);
+    List<Visit> findAllByEmployeeNull();
 }
