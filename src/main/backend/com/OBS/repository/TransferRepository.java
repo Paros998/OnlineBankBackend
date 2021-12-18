13a673ae552d9918a314c418aab7ee4e42ea6407
Dominik Grudzień <62188218+shanodis@users.noreply.github.com>
2021-12-19 00:28:54 +0100

    Transfer filtering refactor

--- src/main/backend/com/OBS/repository/TransferRepository.java
+++ src/main/backend/com/OBS/repository/TransferRepository.java
@@ -2,14 +2,14 @@ package com.OBS.repository;
 
 import com.OBS.entity.Transfer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 @Repository
-public interface TransferRepository extends JpaRepository<Transfer,Long> {
+public interface TransferRepository extends JpaRepository<Transfer,Long>, JpaSpecificationExecutor<Transfer> {
     List<Transfer> findRecentTransfersByClient_clientIdOrderByTransferDateDesc(Long client_id, Pageable pageable);
-
     List<Transfer> findAllByClient_clientId(Long client_id);
 }
